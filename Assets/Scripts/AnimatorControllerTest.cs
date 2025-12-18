4c8bfa53ba85d0920569acb53b2077c5ad6098cc
Yuliya Serzhantova <juliashunina21@gmail.com>
2025-12-18 15:57:02 +0300

    Clean up AnimatorControllerTest.cs

--- Assets/Scripts/AnimatorControllerTest.cs
+++ Assets/Scripts/AnimatorControllerTest.cs
@@ -20,23 +20,11 @@ public class AnimatorControllerTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //float speedInput = _movementInput.y;
-        //float movementSpeed = Mathf.Max(0f, speedInput);
-        //if (_animator != null)
-        //{
-        //    float currentSpeed = _animator.GetFloat("MovementSpeed");
-        //    _animator.SetFloat("MovementSpeed", Mathf.Lerp(currentSpeed, movementSpeed, Time.deltaTime * 10f));
-        //}
         float vertical = _movementInput.y;
 
         if (_animator != null)
         {
-            // Целевое значение скорости, куда мы хотим прийти (0 при отпускании W/S, 1 при нажатии W/S)
             float targetSpeed = Mathf.Abs(vertical);
-
-            // Использование перегруженного метода SetFloat с параметрами damping и deltaTime
-            // 0.1f - это время сглаживания (dampTime) в секундах.
-            // Time.deltaTime - это время, прошедшее с предыдущего кадра.
             _animator.SetFloat("MovementSpeed", targetSpeed, 0.1f, Time.deltaTime);
         }
     }
